d058ba8de415f1c63cefe2fbe6716f244c0d19ee
Kristina Chausheva <52411816+KristinaChausheva@users.noreply.github.com>
2021-04-20 02:32:39 +0300

    styling list and item components

--- next-event/components/events/event-item.js
+++ next-event/components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import styles from './event-item.module.css'
 
 export default function EventItem(props) {
     const { title, image, date, location, id } = props
@@ -14,19 +15,19 @@ export default function EventItem(props) {
     const exploreLink = `/events/${id}`
 
     return (
-        <li>
+        <li className={styles.item}>
             <img src={'/'+ image} alt={title} />
-            <div>
-                <div>
+            <div className={styles.content}>
+                <div className={styles.summary}>
                     <h2>{title}</h2>
-                    <div>
+                    <div className={styles.date}>
                         <time>{humanReadableDate}</time>
                     </div>
-                    <div>
+                    <div className={styles.address}>
                         <address>{formatedAddress}</address>
                     </div>
                 </div>
-                <div>
+                <div className={styles.actions}>
                     <Link href={exploreLink}>Explore Event</Link>
                 </div>
             </div>
