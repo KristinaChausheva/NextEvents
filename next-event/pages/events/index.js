e4efdfa1c287e052d77546eadd8aa898d6ed9309
Kristina Chausheva <52411816+KristinaChausheva@users.noreply.github.com>
2021-04-23 18:56:46 +0300

    Browse all events page

--- next-event/pages/events/index.js
+++ next-event/pages/events/index.js
@@ -1,9 +1,13 @@
-
+import { getAllEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
 
 export default function EventsPage() {
+    const events = getAllEvents()
+
     return (
         <div>
             <h1>Events Page</h1>
+            <EventList items={events}/>
             
         </div>
     )
