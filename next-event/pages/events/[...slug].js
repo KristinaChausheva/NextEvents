ace6da5c60c2e6c660aa01854a772d4b22d4afba
Kristina Chausheva <52411816+KristinaChausheva@users.noreply.github.com>
2021-04-24 18:47:43 +0300

    extracting data from events filter

--- next-event/pages/events/[...slug].js
+++ next-event/pages/events/[...slug].js
@@ -1,5 +1,35 @@
+import { useRouter } from 'next/router'
+import { getFilteredEvents } from '../../dummy-data'
+
 
 export default function FilteredEventsPage() {
+    const router = useRouter()
+    const filterData = router.query.slug
+
+    // console.log(router);
+    // console.log(filterData);
+
+    if(!filterData){
+        return <p>Loading...</p>
+    }
+
+    const filteredYear = +filterData[0]
+    const filteredMonth = +filterData[1]
+
+    // console.log(filteredMonth);
+
+    if(isNaN(filteredYear) || isNaN(filteredMonth) || filteredMonth < 1 || filteredMonth > 12) {
+        return <p>Invalid filter values</p>
+    }
+
+    const filteredEvents = getFilteredEvents({year:filteredYear, month:filteredMonth})
+
+    // console.log(filteredEvents);
+
+    if(!filteredEvents || filteredEvents.length === 0){
+        return <p>no events found for the chosen filter</p>
+    }
+
     return (
         <div>
             <h1>Filtered Events</h1>
